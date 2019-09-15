1724729d7bd0d4d44884e57d81442f88b72e3bef
anupmannem <anupmannem@gmail.com>
2019-09-15 20:48:14 +0530

    using the IIFE pattern

--- modules_basics/dom.js
+++ modules_basics/dom.js
@@ -1,4 +1,4 @@
-function domWrapper(){
+(function(){
 	function addUserToDOM(name) {
 		const node = document.createElement("li");
 		const text = document.createTextNode(name);
@@ -18,6 +18,4 @@ function domWrapper(){
 	for (var i = 0; i < users.length; i++) {
 		addUserToDOM(users[i]);
 	}
-}
-
-domWrapper();
+})()
